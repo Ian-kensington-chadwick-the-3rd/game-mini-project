c944574dc0aedf5d6ceca79092090adb128b84c4
Ezekiel <ezekieljamolin186@gmail.com>
2023-09-29 20:44:04 -0700

    added a guess tracker

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,34 @@ function countdown() {
         timerEl.textContent = '';
         clearInterval(timeInterval);
       }
+
+      
     }, 100);
   }
   
+  var trackW = 0;
+  var trackL = 0;
+  var incrementEl = document.querySelector("#wins");
+  var decrementEl = document.querySelector("#loses");
+  var trackElW = document.querySelector("#trackW");
+  var trackElL = document.querySelector("#trackL");
+
+  function setCounterTextW() {
+    trackElW.textContent = trackW;
+  }
+
+  function setCounterTextL() {
+    trackElL.textContent = trackL;
+  }
+
+  incrementEl.addEventListener("click", function(e) {
+    console.log(e);
+    trackW = trackW + 1;
+    setCounterTextW();
+  });
+
+  decrementEl.addEventListener("click", function(a) {
+    console.log(a);
+    trackL = trackL + 1;
+    setCounterTextL();
+  });
